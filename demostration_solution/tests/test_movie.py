c28126e58f8a63d2271cd738f74a05addd930e07
Михаил <tarqu1w2@gmail.com>
2022-11-03 21:54:03 +0300

    hw20 v 1.1

--- demostration_solution/tests/test_movie.py
+++ demostration_solution/tests/test_movie.py
@@ -1,6 +1,9 @@
 from unittest.mock import MagicMock
 
 import pytest
+
+from dao.model.director import Director
+from dao.model.genre import Genre
 from dao.movie import MovieDAO
 from dao.model.movie import Movie
 from service.movie import MovieService
@@ -8,7 +11,12 @@ from service.movie import MovieService
 
 @pytest.fixture()
 def movie_dao():
+
     movie_dao = MovieDAO(None)
+    director_1 = Director(id=1, name='name_1')
+    genre_1 = Genre(id=1, name='name_1')
+    director_2 = Director(id=2, name='name_2')
+    genre_2 = Genre(id=1, name='name_1')
 
     movie_1 = Movie(id=1,
                     title='title_1',
@@ -17,7 +25,9 @@ def movie_dao():
                     year='2001',
                     rating='6',
                     genre_id='1',
-                    director_id='1')
+                    genre=genre_1,
+                    director_id='1',
+                    director=director_1)
 
     movie_2 = Movie(id=2,
                     title='title_2',
@@ -26,13 +36,15 @@ def movie_dao():
                     year='2002',
                     rating='7',
                     genre_id='2',
-                    director_id='2')
+                    genre=genre_2,
+                    director_id='2',
+                    director=director_2)
 
     movie_dao.get_one = MagicMock(return_value=movie_1)
     movie_dao.get_all = MagicMock(return_value=[movie_1, movie_2])
     movie_dao.create = MagicMock(return_value=Movie(id=3))
     movie_dao.delete = MagicMock(return_value=None)
-    movie_dao.update = MagicMock(return_value=movie_1)
+    movie_dao.update = MagicMock(return_value=movie_2)
 
     return movie_dao
 
@@ -61,12 +73,12 @@ class TestMovieService:
 
         new_movie = self.movie_service.create(movie_data)
         assert new_movie.id is not None
-        assert 3 == new_movie.id
+        assert new_movie.id == 3
 
     def test_update(self):
         movie_data = {'id': 2,
                       'title': 'title_2',
-                      'description': 'new_description',
+                      'description': 'description_2',
                       'trailer': 'trailer_2',
                       'year': '2002',
                       'rating': '7',
@@ -75,7 +87,7 @@ class TestMovieService:
 
         new_movie = self.movie_service.update(movie_data)
         assert new_movie.id is not None
-        assert 'new_description' == new_movie.description
+        assert new_movie.description == 'description_2'
 
     def test_delete(self):
         assert self.movie_service.delete(1) == None
